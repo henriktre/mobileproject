6af3f07ace8d4b358ecf04ec0be79606757a63c6
stian <stianbso@stud.ntnu.no>
2018-05-02 00:06:10 +0200

    navigation +empty shell for profile and search

---
+++ app/src/main/java/com/mobileproject/Search.java
@@ -0,0 +1,19 @@
+package com.mobileproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Stian on 30.04.2018.
+ */
+
+public class Search extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        //Toolbar toolbar = findViewById(R.id.my_toolbar);
+        //setSupportActionBar(toolbar);
+    }
+}
